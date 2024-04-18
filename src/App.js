11f0e6483173d99ea13e7aaf530f18e31be593b6
Azam Amonov <azamamonov555@gmail.com>
2024-04-19 04:27:51 +0500

    Add Category From

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // App.js
 import React, { useState } from 'react';
-import Table from './Tables/./Table';
+import Table from './Tables/Table';
 import AddRecordForm from './CRUD/AddRecordForm';
-import CategoryTable from "./Tables/CategoryTable";
-import RecordTable from "./Tables/RecordTable";
+import AddCategoryForm from './CRUD/AddCategoryForm';
+import CategoryTable from './Tables/CategoryTable';
+import RecordTable from './Tables/RecordTable';
 
 function App() {
   const [data, setData] = useState([]);
@@ -15,12 +16,17 @@ function App() {
   const deleteRecord = (id) => {
     setData(data.filter((record) => record.id !== id));
   };
-
+  
+  const addCategory = (id) => {
+    setData(data.filter((record) => record.id));
+  }
+  
   return (
       <div className={"app-container"} align={"center"}>
         <h1>CRUD Table</h1>
           <div className= "cneter-contanier" align={"center"}>
               <AddRecordForm addRecord={addRecord} />
+              <AddCategoryForm addCategory = {addCategory}/>
               <RecordTable data={data} deleteRecord={deleteRecord} />
               <Table data={data} deleteRecord={deleteRecord} />
               <CategoryTable data={data} deleteRecord={deleteRecord}/>
