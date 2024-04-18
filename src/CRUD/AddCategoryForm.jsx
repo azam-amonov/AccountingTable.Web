d05df21046186b7076cc93c52c3d002ddb6ce8a6
Azam Amonov <azamamonov555@gmail.com>
2024-04-19 03:50:08 +0500

    Add Category From

---
+++ src/CRUD/AddCategoryForm.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+import uuid from "react-uuid";
+
+function AddCategoryForm({addCategory}){
+    const [name, setName] = useState()
+    const [accounting, setAccounting] = useState()    
+}
