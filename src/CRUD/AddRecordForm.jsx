717937621d6461d7c0b8fc39e439433a9257d26e
Azam Amonov <azamamonov555@gmail.com>
2024-04-18 23:09:32 +0500

    Add categories json

--- src/CRUD/AddRecordForm.jsx
+++ src/CRUD/AddRecordForm.jsx
@@ -1,15 +1,21 @@
 // AddRecordForm.jsx
 import React, {useEffect, useState } from 'react';
 import uuid from "react-uuid";
+import categoriesData from '../data/categories.json';
 
 function AddRecordForm({ addRecord }) {
     const currentDate = new Date().toISOString().slice(0,16)
     const [type, setType] = useState('');
+    const [categories, setCategories] = useState([])
     const [category, setCategory] = useState('');
     const [date, setDate] = useState(currentDate);
     const [amount, setAmount] = useState('');
     const [comment, setComment] = useState('');
 
+    useEffect(() => {
+        setCategories(categoriesData); // Set categories data from imported JSON
+    }, []);
+    
     const handleSubmit = (e) => {
         e.preventDefault();
         const id = uuid(); // Generate unique ID
@@ -29,12 +35,15 @@ function AddRecordForm({ addRecord }) {
                 <option value= '0'> Income </option>
                 <option value= '0'> Expenses </option>
             </select> &nbsp;
-            <input
-                type="text"
-                placeholder="Category"
+            <select
                 value={category}
                 onChange={(e) => setCategory(e.target.value)}
-            /> &nbsp;
+            >
+                <option value=''> Select Category</option>
+                {categories.map(cat => (
+                    <option key={cat.id} value={cat.name}>{cat.name}</option>
+                ))}
+            </select> &nbsp;
             <input
                 type="datetime-local"
                 placeholder="Date"
