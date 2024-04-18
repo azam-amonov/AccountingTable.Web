e28292043a1c00b7e6b57806a103faf0335f1cb1
Azam Amonov <azamamonov555@gmail.com>
2024-04-18 19:14:34 +0500

    Configure App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import Table from './Tables/./Table';
+import AddRecordForm from './CRUD/AddRecordForm';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const addRecord = (record) => {
+    setData([...data, record]);
+  };
+
+  const deleteRecord = (id) => {
+    setData(data.filter((record) => record.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"app-container"} align={"center"}>
+        <h1>CRUD Table</h1>
+          <div className= "cneter-contanier" align={"center"}>
+              <AddRecordForm addRecord={addRecord} />
+              <Table data={data} deleteRecord={deleteRecord} />
+          </div>
+        
+      </div>
   );
 }
 
