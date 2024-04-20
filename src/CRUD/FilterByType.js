73c9215568ec61589fc4f44ae69528a7a717ff6e
Azam Amonov <azamamonov555@gmail.com>
2024-04-20 19:38:48 +0500

    Filter By Type

--- src/CRUD/FilterByType.js
+++ src/CRUD/FilterByType.js
@@ -1,32 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import "react-datepicker/dist/react-datepicker.css";
-import './Form.css';
 import axios from "axios";
 
 function FilterByType(){
+    const [accounting, setAccounting] = useState('');
     const [typedTransaction, setTypedTransaction] = useState([]);
-    useEffect(() => {
-        
-    }, []);
+    
+    const handleTypeChange = (e) => {
+        setAccounting(e.target.value)
+    }
     
     const onGetType = () => {
-        axios.get(`https://localhost:5177/api/TransactionResutl/${accounting}`)
-            .then(() =>{
-            })
-            
+        if (!accounting){
+            console.error('Please select a type')
+            return;
         }
+        
+        axios.get(`https://localhost:5177/api/TransactionResult/type/${accounting}`)
+            .then((response) =>{
+                setTypedTransaction(response.data);
+                console.log(typedTransaction);
+            })
+            .catch((error) => {
+                console.error('Error fetching transaction: ', error);
+            })
+        };
     
     return (
         <form action="">
             <select
                 className="select-accounting"
                 style={{fontSize: '23px', padding: '10px', width: '10%'}}
+                value={accounting}
+                onChange={handleTypeChange}
             >
                 <option value="">Select Type</option>
-                <option value={parseInt('0')}> Income </option>
-                <option value={parseInt('1')}> Expenses </option>
-            </select> &nbsp; &nbsp;
-            <button type='button'> Sort Type</button>
+                <option value='0'> Income</option>
+                <option value='1'> Expenses</option>
+            </select>
+            &nbsp; &nbsp;
+            <button type='button' onClick={onGetType}> Sort Type</button>
         </form>
 
     )
