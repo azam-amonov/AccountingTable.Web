176c9349a86340d8a9cada06532c4a4b0f76458b
Azam Amonov <azamamonov555@gmail.com>
2024-04-24 13:03:26 +0500

    Add TransactionContext

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import {TransactionResultProvider} from "./context/TransactionResultContext";
 import FilterByType from "./copmonents/TransactionsList/FilterByType";
 import FilterByDate from "./copmonents/TransactionsList/FilterByDate";
 import {CategoryProvider} from "./context/CategoryContext";
+import {TransactionProvider} from "./context/TransactionContext";
 
 function App() {
     return (
         <TransactionResultProvider>
+            <TransactionProvider>
             <CategoryProvider>
             <div className={"app-container"} align={"center"}>
                 <h1>Micro Accounting</h1>
@@ -24,6 +26,7 @@ function App() {
                 <Table/>
             </div>
             </CategoryProvider>
+            </TransactionProvider>
         </TransactionResultProvider>
     );
 }
