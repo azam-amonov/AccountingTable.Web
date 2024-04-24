cbd625f46fa6360ac213b4862b645e9bbbab40d4
Azam Amonov <azamamonov555@gmail.com>
2024-04-24 11:47:53 +0500

    CODE RUB: CleanUp

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import AddTransactionsForm from './copmonents/AddTransaction/AddTransactionsForm
 import AddCategoryForm from './copmonents/AddCategory/AddCategoryForm';
 import DeleteCategoryForm from "./copmonents/DeleteCategory/DeleteCategoryForm";
 import FilterByCategory from "./copmonents/TransactionsList/FilterByCategory";
-import {TransactionProvider} from "./context/TransactionsContext";
+import {TransactionResultProvider} from "./context/TransactionResultContext";
 import FilterByType from "./copmonents/TransactionsList/FilterByType";
 import FilterByDate from "./copmonents/TransactionsList/FilterByDate";
 import {CategoryProvider} from "./context/CategoryContext";
 
 function App() {
     return (
-        <TransactionProvider>
+        <TransactionResultProvider>
             <CategoryProvider>
             <div className={"app-container"} align={"center"}>
                 <h1>Micro Accounting</h1>
@@ -24,7 +24,7 @@ function App() {
                 <Table/>
             </div>
             </CategoryProvider>
-        </TransactionProvider>
+        </TransactionResultProvider>
     );
 }
 
