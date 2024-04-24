73b41f44b5e8cf8a6ec680d88f6f8ca020d2367a
Azam Amonov <azamamonov555@gmail.com>
2024-04-24 18:38:00 +0500

    Add Update Table

--- src/tables/Table.jsx
+++ src/tables/Table.jsx
@@ -20,6 +20,10 @@ function Table() {
         fetchTransactionResult();
     };
     
+    const onUpdate = (transaction) => {
+        updateTransaction(transaction); 
+    }
+
     if (filteredTransactionResults.length > 0) {
         displayTransactions = filteredTransactionResults;
     } else {
@@ -29,36 +33,105 @@ function Table() {
 
     return (
         <div>
-        <table>
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Type</th>
-                <th>Category</th>
-                <th>Date</th>
-                <th>Amount</th>
-                <th>Comment</th>
-                <th>Action</th>
-            </tr>
-            </thead>
-            <tbody>
-            {displayTransactions.map(({transaction, category}) => (
-                <tr key={transaction.id}>
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Type</th>
+                    <th>Category</th>
+                    <th>Date</th>
+                    <th>Amount</th>
+                    <th>Comment</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {displayTransactions.map(({transaction, category}) => (
+                    updateState === transaction.id ?
+                        <Edit transaction = {transaction} 
+                              transactions = {transactions} 
+                              category = {category}
+                              setTransactions = {setTransactions} 
+                              onUpdate ={onUpdate} 
+                        /> :
+                    <tr key={transaction.id}>
+                        <td>{transaction.id}</td>
+                        <td>{category.accounting === 0 ? 'Income' : 'Expenses'}</td>
+                        <td>{category.name}</td>
+                        <td>{new Date(transaction.transactionDate).toLocaleDateString()}</td>
+                        <td>{numberToCurrency(transaction.amount)}</td>
+                        <td>{transaction.comment}</td>
+                        <td>
+                            <button onClick={() => handleEdit(transaction.id)}>Edit</button>
+                            &nbsp; | &nbsp;
+                            <button onClick={() => handleDelete(transaction.id)}>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+    
+    function handleUpdate(e){
+        e.preventDefault();
+    }
+    function handleEdit(id) {
+        setUpdateState(id)
+    }
+
+    function Edit({transaction, category, transactions, setTransactions, onUpdate}){
+        const [editedTransaction, setEditedTransaction] = useState(transaction);
+        
+        function handleChange(e){
+            setEditedTransaction({
+                ...editedTransaction,
+                [e.target.name] : e.target.value,
+            })
+        }
+
+        function handleSubmit(e){
+            e.preventDefault();
+            onUpdate(editedTransaction)
+            console.log(editedTransaction)
+            setUpdateState(null);
+        }
+        return(
+                <tr>
                     <td>{transaction.id}</td>
                     <td>{category.accounting === 0 ? 'Income' : 'Expenses'}</td>
                     <td>{category.name}</td>
-                    <td>{new Date(transaction.transactionDate).toLocaleDateString()}</td>
-                    <td>{numberToCurrency(transaction.amount)}</td>
-                    <td>{transaction.comment}</td>
                     <td>
-                        <button onClick={() => onDelete(transaction.id)}>Delete</button>
+                        <input type="date" 
+                               name="transactionDate" 
+                               onChange={handleChange} 
+                               value={editedTransaction.transactionDate}/>
+                    </td>
+                    <td>
+                        <input type="number"
+                               name="amount"
+                               placeholder="Amount"
+                               onChange={handleChange} 
+                               value={editedTransaction.amount}/></td>
+                    <td>
+                        <li>
+                            <input type="text" 
+                                   name="comment" 
+                                   placeholder='Comment'
+                                   onChange={handleChange} 
+                                   value={editedTransaction.comment}/>
+                        </li>
+                    </td>
+                    <td>
+                        <button type="submit" onClick={handleSubmit}> Update </button>
                     </td>
                 </tr>
-            ))}
-            </tbody>
-        </table>
-        </div>
-    );
-}
+        )
+    }
+    
+    
+    
+};
+
 
 export default Table;
